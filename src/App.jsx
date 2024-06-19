aaee64f9ba745c3162263559b63016c4648a6a5f
mhuss03 <134786667+mhuss03@users.noreply.github.com>
2024-06-19 12:08:06 +0100

    Added new recitor

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,51 @@ import { useState, useRef, useEffect } from "react";
 
 function App() {
   const [start, setStart] = useState("1");
-  const [end, setEnd] = useState("34");
+  const [end, setEnd] = useState("35");
   const [isPlaying, setIsPlaying] = useState(false);
   const [playCount, setPlayCount] = useState(0);
   const [playbackSpeed, setPlaybackSpeed] = useState(1);
+  const [recitor, setRecitor] = useState(1);
 
   const audioRef = useRef(null);
   const intervalRef = useRef(null);
 
   const timeStamp = {
-    1: 0,
-    2: 8,
-    3: 14.4,
-    4: 20.7,
-    5: 27.64,
-    6: 34.3,
-    7: 40.7,
-    8: 46.9,
-    9: 53.3,
-    10: 60,
-    11: 66.7,
-    12: 73.5,
-    13: 79.9,
-    14: 86,
-    15: 93.1,
-    16: 99.6,
-    17: 106.3,
-    18: 112.6,
-    19: 118.8,
-    20: 125.3,
-    21: 132.1,
-    22: 138.8,
-    23: 145,
-    24: 151.6,
-    25: 158.3,
-    26: 164.4,
-    27: 170.7,
-    28: 177.7,
-    29: 184,
-    30: 190.5,
-    31: 197,
-    32: 203.7,
-    33: 210,
-    34: 221,
+    1: [0, 0],
+    2: [8, 9.06],
+    3: [14.4, 15],
+    4: [20.7, 21.8],
+    5: [27.64, 28.54],
+    6: [34.3, 35.16],
+    7: [40.7, 41.89],
+    8: [46.9, 48.44],
+    9: [53.3, 55.43],
+    10: [60, 62.475],
+    11: [66.7, 69.615],
+    12: [73.5, 76.36],
+    13: [79.9, 82.715],
+    14: [86, 89.615],
+    15: [93.1, 96.314],
+    16: [99.6, 103.258],
+    17: [106.3, 110.1],
+    18: [112.6, 116.854],
+    19: [118.8, 123.675],
+    20: [125.3, 130.262],
+    21: [132.1, 137.05],
+    22: [138.8, 143.94],
+    23: [145, 150.9],
+    24: [151.6, 157.8],
+    25: [158.3, 164],
+    26: [164.4, 170.25],
+    27: [170.7, 176.82],
+    28: [177.7, 183.24],
+    29: [184, 189.9],
+    30: [190.5, 195.86],
+    31: [197, 202.66],
+    32: [203.7, 209.21],
+    33: [210, 215.815],
+    34: [221, 222.42],
+    35: [231, 229.04],
   };
 
   useEffect(() => {
@@ -77,14 +79,14 @@ function App() {
         clearInterval(intervalRef.current);
       }
     } else {
-      audioRef.current.currentTime = timeStamp[startInt];
+      audioRef.current.currentTime = timeStamp[startInt][recitor - 1];
       audioRef.current.playbackRate = playbackSpeed;
       audioRef.current.play();
       setIsPlaying(true);
       setPlayCount(1);
 
       intervalRef.current = setInterval(() => {
-        if (audioRef.current.currentTime >= timeStamp[endInt]) {
+        if (audioRef.current.currentTime >= timeStamp[endInt][recitor - 1]) {
           if (playCount >= 5) {
             clearInterval(intervalRef.current);
             audioRef.current.pause();
@@ -92,7 +94,7 @@ function App() {
             setPlayCount(0);
           } else {
             audioRef.current.pause();
-            audioRef.current.currentTime = timeStamp[startInt];
+            audioRef.current.currentTime = timeStamp[startInt][recitor - 1];
             audioRef.current.play();
             setPlayCount((prevCount) => prevCount + 1);
           }
@@ -104,7 +106,14 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center bg-gray-100 p-4">
-        <audio ref={audioRef} src="assets/audio-files/full-audio.mp3" />
+        <audio
+          ref={audioRef}
+          src={
+            recitor === 1
+              ? "assets/audio-files/full-audio-1.mp3"
+              : "assets/audio-files/full-audio-2.mp3"
+          }
+        />
         <div className="flex space-x-4 mb-4">
           <div>
             <select
@@ -126,7 +135,7 @@ function App() {
                 </option>
               ))}
             </select>
-            <h3 className="text-center">Upto</h3>
+            <h3 className="text-center">From</h3>
           </div>
           <div>
             <select
@@ -142,13 +151,13 @@ function App() {
                 }
               }}
             >
-              {Array.from({ length: 34 }, (_, i) => (
+              {Array.from({ length: 35 }, (_, i) => (
                 <option key={i + 1} value={i + 1}>
                   {i + 1}
                 </option>
               ))}
             </select>
-            <h3 className="text-center">From</h3>
+            <h3 className="text-center">to</h3>
           </div>
           <button
             onClick={() => handlePlayPause(start)}
@@ -157,13 +166,16 @@ function App() {
             {isPlaying ? "Pause" : "Play"}
           </button>
         </div>
-        <div className="flex space-x-4 mb-4">
-          <label htmlFor="playbackSpeed" className="text-gray-700">
+        <div className="flex w-1/3 gap-6">
+          <label
+            htmlFor="playbackSpeed"
+            className="block mb-2 text-sm font-medium text-gray-900 my-auto"
+          >
             Playback Speed:
           </label>
           <select
             id="playbackSpeed"
-            className="border-2 border-gray-400 rounded-lg p-2 focus:outline-none focus:border-blue-500 shadow-sm"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mr-4"
             value={playbackSpeed}
             onChange={(e) => setPlaybackSpeed(parseFloat(e.target.value))}
           >
@@ -175,6 +187,22 @@ function App() {
             <option value="1.25">1.25x</option>
             <option value="1.5">1.5x</option>
             <option value="2">2x</option>
+          </select>
+          <label
+            htmlFor="recitor"
+            className="block mb-2 text-sm font-medium text-gray-900 my-auto"
+          >
+            Choose Recitor
+          </label>
+          <select
+            name=""
+            id="recitor"
+            value={recitor}
+            onChange={(e) => setRecitor(parseInt(e.target.value))}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          >
+            <option value="1">Badr Al-Turki</option>
+            <option value="2">Sayyid Samdani</option>
           </select>
         </div>
       </div>
